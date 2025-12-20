953f8621bdaf9170609b4b5f0285f20c568ff10e
Rushui Guan <rushui@outlook.com>
2025-12-20 15:14:10 -0500

    update test ref

--- Sample.CodeGen/MyCodeGen.cs
+++ Sample.CodeGen/MyCodeGen.cs
@@ -10,17 +10,7 @@ namespace Sample.CodeGen {
 			context.RegisterSourceOutput(
 				context.CompilationProvider,
 				(ctx, compilation) => {
-					var cs = new CodeStack();
-					using (cs.NewScope(new CompilationUnitBuilder())) {
-						using (cs.NewScope(new NamespaceDeclarationBuilder("Sample.CodeGen"))) {
-							using (cs.NewScope(new ClassDeclarationBuilder("MyTest").Public())) {
-								using (cs.NewScope(new MethodDeclarationBuilder("void", "MyMethod").Public())) {
-									cs.Begin(new VariableBuilder("string", "test1")).With(new LiteralNode("MyTest".Kebaberize())).End();
-								}
-							}
-						}
-					}
-					ctx.AddSource("MyTest", SourceText.From(cs.Build(), Encoding.UTF8));
+					ctx.AddSource("MyTest", SourceText.From("public class Test { }", Encoding.UTF8));
 				}
 			);
 		}
